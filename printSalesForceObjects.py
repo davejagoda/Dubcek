8901111241c986022baf36615e393c9e3b727bb8
dj <davejagoda@users.noreply.github.com>
2016-10-18 16:56:28 -0700

    Update printSalesForceObjects.py to print SFIDs

--- printSalesForceObjects.py
+++ printSalesForceObjects.py
@@ -21,22 +21,44 @@ def loginToSalesForce(isProduction):
         username = u, password = p, security_token = t, sandbox = sandbox)
     return(salesforce)
 
-def walkAccounts(salesforce):
+def walkAccounts(salesforce, sfid):
+    result = []
     allaccounts = salesforce.query_all('select Id, Name from Account')
     for record in allaccounts['records']:
-        print(record['Name'].encode('utf8'))
+        if sfid:
+            tuple = (record['Id'], record['Name'])
+        else:
+            tuple = (record['Name'])
+        result.append(tuple)
+    return(result)
 
-def walkContacts(salesforce):
+def walkContacts(salesforce, sfid):
+    result = []
     allcontacts = salesforce.query_all('select Id, Name, Email from Contact')
     for record in allcontacts['records']:
-        try:
-            print(record['Name'].encode('utf8') + ':' + record['Email'])
-        except:
-            print(record['Name'].encode('utf8') + ':')
+        if record['Email'] is None:
+            email = 'NO EMAIL'
+        else:
+            email = record['Email']
+        if sfid:
+            tuple = (record['Id'], record['Name'], email)
+        else:
+            tuple = (record['Name'], email)
+        result.append(tuple)
+    return(result)
+
+def print_nicely(result):
+    broken_bar = unichr(0x00a6)
+    for row in result:
+        if (str == type(row) or unicode == type(row)):
+            print(row)
+        else:
+            print(broken_bar.join(row))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--production', action='store_true', help='use production SalesForce instance')
+    parser.add_argument('--sfid', action='store_true', help='display SalesForce ID')
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument('--account', action='store_true')
     group.add_argument('--contact', action='store_true')
@@ -47,6 +69,8 @@ if __name__ == '__main__':
         print('using test instance')
     salesforce = loginToSalesForce(args.production)
     if args.account:
-        walkAccounts(salesforce)
+        result = walkAccounts(salesforce, args.sfid)
     if args.contact:
-        walkContacts(salesforce)
+        result = walkContacts(salesforce, args.sfid)
+    print(len(result))
+    print_nicely(result)
